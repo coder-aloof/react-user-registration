d5c93542e81dad9ceee6a677c2cb91015fa32efb
Nirmal Kumar <codingboredom@gmail.com>
2020-03-09 01:11:59 +0530

    Updates

---
+++ precache-manifest.8596a532b25ea57ef63a585ed66e1f63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7560c18a7c6d2f293f7853d895ad49d8",
+    "url": "/react-user-registration/index.html"
+  },
+  {
+    "revision": "2aa96d1ac76b5fb1167b",
+    "url": "/react-user-registration/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "115a0022e0aab98ec6f2",
+    "url": "/react-user-registration/static/css/main.029d5e2c.chunk.css"
+  },
+  {
+    "revision": "2aa96d1ac76b5fb1167b",
+    "url": "/react-user-registration/static/js/2.5f2556a9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-user-registration/static/js/2.5f2556a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115a0022e0aab98ec6f2",
+    "url": "/react-user-registration/static/js/main.cee3c33a.chunk.js"
+  },
+  {
+    "revision": "ab32329fb7711a3ffefc",
+    "url": "/react-user-registration/static/js/runtime-main.20b035f1.js"
+  }
+]);
